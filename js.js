54f0725d509b32b3491481066eac9ce372661dee
Elvin2002 <elvinvarasmeregildo@gmail.com>
2023-12-19 17:25:01 -0500

    commit 4

---
+++ js.js
@@ -0,0 +1,17 @@
+{
+    "slides": ["/img/juego_muebles.webp",
+        "/img/renders_page-0001.webp",
+        "/img/FOTO_24.webp",
+        "/img/imagengrande3.webp",
+        "/img/img1.webp",
+        "/img/img2.webp",
+        "/img/imggrande1.webp",
+        "/img/img1800x600-.webp",
+        "/img/Cebra_Acostada.webp",
+        "/img/Butaca_con_Acero.webp",
+        "/img/elefante.webp",
+        "/img/FOTO_27.webp",
+        "/img/FOTO_26.webp",
+        "/img/imagengrande3.webp"
+    ]
+}
